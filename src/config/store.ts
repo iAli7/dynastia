1a0b8f4fa58375778561ff5ca9634891f6479d7b
iAli7 <aligokyer206@gmail.com>
2023-06-18 00:02:30 +0300

    update

--- src/config/store.ts
+++ src/config/store.ts
@@ -2,8 +2,8 @@ import { ref } from "vue"
 
 const store = {
     population: ref(1),
-    happiness: ref(-20),
-    food: ref(-25)
+    happiness: ref(0),
+    food: ref(5)
 }
 
 export default store
